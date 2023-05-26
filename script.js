53f2fada4f6008e676cf6122020ee93af46f9686
lohit4803 <104892757+lohit4803@users.noreply.github.com>
2023-05-26 13:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function calculateMinCost() {
-  //your code here
+
+  function calculateMinCost(arr){
+  let sum = 0
+  let n = arr.length
+
+  while(n>1){
+    arr.sort()
+    let firstTwo = arr[0]+arr[1]
+    sum+=firstTwo
+    arr.splice(0,2,firstTwo)
+    n--
+  }
+
+  console.log(sum)
+}
+
+let arr = [4,2,7]
+calculateMinCost(arr)
   
   
   
